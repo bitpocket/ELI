91228b3adac8ad273604680e5c9cdcb021de64ab
bitpocket <a@b.c>
2015-11-20 23:57:39 +0100

    initial commit

---
+++ ELI/ELI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELI
+{
+    public class ELI
+    {
+        /// <summary>
+        /// test description
+        /// </summary>
+        /// <returns>test retrun</returns>
+        public List<string> GetPersons()
+        {
+            List<string> result = new List<string>();
+
+            result.Add("Person1");
+            result.Add("Person2");
+            result.Add("Person3");
+
+            return result;
+        }
+
+        /// <summary>
+        /// 2222222 
+        /// </summary>
+        /// <param name="data"> dasat 2222</param>
+        /// <returns>sdsdfgsd ret</returns>
+        public Boolean SendSomeData(string data)
+        {
+            return true;
+        }
+    }
+}
