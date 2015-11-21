5b69d8875f0c416bcd7aa56988fac44494d39bec
bitpocket <a@b.c>
2015-11-21 01:46:45 +0100

    dll was used successfully i fox pro

--- ELI/ELI.cs
+++ ELI/ELI.cs
@@ -4,8 +4,35 @@ using System.Runtime.InteropServices;
 
 namespace ELI
 {
+
+    [Guid("C4B4BD32-01F4-4609-9B46-020A48A7EE06")]
+    [ComVisible(true)]
+    public interface IFoo
+    {
+        string DoFoo();
+    }
+
     [ClassInterface(ClassInterfaceType.AutoDual)]
+    [Guid("737C975F-C910-45E5-89B3-2A53991FB0F2")]
+    [ComVisible(true)]
+    [ProgId("ELI.Foo")]
+    public class Foo : IFoo
+    {
+        public Foo()
+        {
+        }
+
+        public string DoFoo()
+        {
+            return "some foo";
+        }
+    }
+
+
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [Guid("7C82DE98-324C-48B3-B51D-03F785D6B6B9")]
     [ComSourceInterfaces(typeof(MyEvents))]
+    [ComVisible(true)]
     [ProgId("ELI.WebServiceShell")]
     public class WebServiceShell
     {
@@ -33,9 +60,16 @@ namespace ELI
         {
             return true;
         }
+
+        public string HelloWorld(string name)
+        {
+            return "Hello World " + name;
+        }
     }
 
     [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    [Guid("6339B265-EC1A-4444-AF9E-A11CB2FD530A")]
+    [ComVisible(true)]
     public interface MyEvents
     {
         //the comm interface for the eventhandler IsCompleted
