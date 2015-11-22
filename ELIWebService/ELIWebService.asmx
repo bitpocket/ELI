4040a41d9f8c481475bfc3eb6ee94fab4cc1a1a3
bitpocket <a@b.c>
2015-11-22 02:22:05 +0100

    web service initial commit

---
+++ ELIWebService/ELIWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ELIWebService.asmx.cs" Class="ELIWebService.ELIWebService" %>
---
+++ ELIWebService/ELIWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ELIWebService
+{
+    /// <summary>
+    /// Summary description for ELIWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ELIWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld(string param)
+        {
+            return "Hello World " + param;
+        }
+    }
+}
